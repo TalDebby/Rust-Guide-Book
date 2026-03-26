bb0bde8cc6721eba529ab63788846e0898e43a54
Tal Debby <taldebby2004@gmail.com>
2026-03-26 16:18:34 +0200

    feat(lib.rs): search logic with TDD

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,27 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!()
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "ung";
+        let contents = "\
+Rust:
+The rise of the jungle.
+Pick three.";
+
+        assert_eq!(vec!["The rise of the jungle."], search(query, contents));
+    }
 }
